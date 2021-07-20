e24914b3ff18821156553834fcf6ab28fdc3d9e5
SEAGAMES <32480062+SEAGAMES@users.noreply.github.com>
2021-07-20 15:24:24 +0700

    initial commit

--- backend_dummy/api_product.js
+++ backend_dummy/api_product.js
@@ -5,28 +5,37 @@ router.get("/product", (req, res)=>{
     res.json([
     {
         _id: "5sdadad8dadsd8wdadw" ,
-        id: 11,
+        id: 01,
         name: "Arduino Nano 3.0",
-        price: 11,
-        stock: 130,
+        price: 300,
+        stock: 50,
         created: "2018-03-30T00:11:45.109Z",
         __v: 0
     },   
     { 
-        _id: "5sdadad8dadsd8wdadw" ,
-        id: 11,
-        name: "Arduino Nano 3.0",
-        price: 11,
-        stock: 130,
+        _id: "sadsadafsaffasfggfga" ,
+        id: 02,
+        name: "mouse logitech",
+        price: 250,
+        stock: 100,
         created: "2018-03-30T00:11:45.109Z",
         __v: 0
     }, 
     {
         _id: "5sdadad8dadsd8wdadw" ,
-        id: 11,
-        name: "Arduino Nano 3.0",
-        price: 11,
-        stock: 130,
+        id: 03,
+        name: "keyboard",
+        price: 199,
+        stock: 100,
+        created: "2018-03-30T00:11:45.109Z",
+        __v: 0
+    }, 
+    {
+        _id: "dgsGdgSSFFAFfe33fsdv" ,
+        id: 5,
+        name: "Monitor",
+        price: 4300,
+        stock: 50,
         created: "2018-03-30T00:11:45.109Z",
         __v: 0
     }, 
--- src/views/Stock.vue
+++ src/views/Stock.vue
@@ -1,5 +1,6 @@
 <template>
     <v-container>
+        <!-- Summary section -->
         <v-row>
             <v-col lg="3" md="6" sm="12" col="12"> 
                 <StockCard title="TOTAL" subtitle="1,800" avatar_bg="#00a65a" avatar_ic="mdi-cart-outline" />
@@ -17,6 +18,35 @@
                 <StockCard title="DISCOUNT" subtitle="101" avatar_bg="#00c0ef" avatar_ic="mdi-gift-outline" />
             </v-col>
         </v-row>
+
+        <!-- Table section -->
+        <v-card>
+            <v-data-table
+                :headers="headers"
+                :items="mDataArray"
+            >
+            <template v-slot:item="{ item }">
+                <tr>
+                    <td>{{ item.id }}</td>
+                    <td>{{ item.image }}</td>
+                    <td>{{ item.name }}</td>
+                    <td>{{ item.price | currency("฿") }}</td>
+                    <td>{{ item.stock | number("0,0") }} pcs.</td>
+                    <td>xxx</td>
+                    <td>
+                     <v-icon class="ma-2" @click="editItem(item)">
+                         edit
+                     </v-icon>
+                     <span class="mr-1"></span>
+                     <v-icon @click="deleteIte(item)">
+                         delete
+                     </v-icon>
+                     </td>
+                </tr>
+            </template>
+            </v-data-table>
+        </v-card>
+
     </v-container>
 </template>
 
@@ -26,6 +56,25 @@ import StockCard from "@/components/cards/StockCard"
 import Axios from "axios"
 export default {
     name : "Stock" , 
+    data() {
+        return {
+            mDataArray:[],
+                headers: [
+                    {
+                    text: 'Id',
+                    align: 'left',
+                    sortable: false,
+                    value: 'id',
+                    },
+                    {  text: 'Image', value: 'image' },
+                    {  text: 'Name', value: 'name' },
+                    {  text: 'Price', value: 'price' },
+                    {  text: 'Stock', value: 'stock' },
+                    {  text: 'Action', value: 'action' },
+
+                ],
+        }
+    },
     components : {
         StockCard
     },
@@ -33,6 +82,7 @@ export default {
         console.log("Stock component is run"),
         Axios.get("http://localhost:8081/api/v2/product").then((result)=>{
             console.log(JSON.stringify(result.data))
+            this.mDataArray = result.data
         })
     }
 
